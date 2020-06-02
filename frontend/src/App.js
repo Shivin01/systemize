a43007bd840ad1913e0f5106a1b9641814bdb4e0
immanoj16 <mmaharana14@gmail.com>
2020-06-02 08:07:16 +0530

    Add modal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+
 import Login from './components/Login'
+import Signup from './components/Signup'
+import Home from './components/Home'
+import NavBar from './components/NavBar'
 
 function App() {
-  return <Login />
+  const [active, setActive] = useState(false)
+
+  return (
+    <Router>
+      <NavBar active={active} setActive={setActive} />
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/signup">
+          <Signup />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App
---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,74 @@
+/** @jsx jsx */
+/** @jsxFrag React.Fragment */
+import {css, jsx} from '@emotion/core'
+import {useState, Fragment} from 'react'
+import PropTypes from 'prop-types'
+
+import Logo from '../images/Logo.svg'
+import Modal from './Modal'
+
+function NavBar({active, setActive}) {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <Fragment>
+      <div className="w-screen h-auto shadow-xl">
+        <div className="container m-auto">
+          <nav className="flex items-center justify-between flex-wrap py-4">
+            <a
+              className="no-underline pr-8"
+              href="#fdsf"
+              css={css`
+                border-right: 2px solid rgba(0, 0, 0, 0.5);
+              `}
+            >
+              <img src={Logo} alt="logo" />
+            </a>
+            <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto pl-8">
+              <div className="lg:flex-grow">
+                <a
+                  href="#responsive-header"
+                  className="inline-block mt-4 lg:mt-0 mr-4"
+                  style={
+                    active
+                      ? {borderBottom: '3px solid #000000', paddingBottom: 3}
+                      : {}
+                  }
+                  onClick={() => setActive(true)}
+                >
+                  Your Tasks
+                </a>
+              </div>
+              <div className="mr-4">
+                <button
+                  className="text-white font-bold py-2 px-12 rounded focus:outline-none focus:shadow-outline"
+                  type="button"
+                  onClick={() => setIsOpen(true)}
+                  css={css`
+                    background: linear-gradient(
+                      264.33deg,
+                      #7ee0ef 0%,
+                      #15aad9 100%
+                    );
+                    box-shadow: 0px 15px 20px rgba(32, 175, 221, 0.34);
+                  `}
+                >
+                  Create
+                </button>
+              </div>
+              <div className="w-12 h-12 rounded-full bg-green-900 border-gray-600 border-4 ml-4 shadow-sm" />
+            </div>
+          </nav>
+        </div>
+      </div>
+      {isOpen ? <Modal setIsOpen={setIsOpen} /> : null}
+    </Fragment>
+  )
+}
+
+NavBar.propTypes = {
+  active: PropTypes.bool.isRequired,
+  setActive: PropTypes.func.isRequired,
+}
+
+export default NavBar

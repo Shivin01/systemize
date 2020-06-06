3c039838411b2415bd5e56532d20c1860b5e8d23
immanoj16 <mmaharana14@gmail.com>
2020-06-06 11:28:38 +0530

    Add Profile page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,19 @@
 import React, {useState} from 'react'
+import PropTypes from 'prop-types'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 import Login from './components/Login'
 import Signup from './components/Signup'
 import Home from './components/Home'
 import NavBar from './components/NavBar'
+import Profile from './components/Profile'
 
-function App() {
+function App({loggedIn = true}) {
   const [active, setActive] = useState(false)
 
   return (
     <Router>
-      <NavBar active={active} setActive={setActive} />
+      {loggedIn && <NavBar active={active} setActive={setActive} />}
       <Switch>
         <Route path="/login">
           <Login />
@@ -19,12 +21,19 @@ function App() {
         <Route path="/signup">
           <Signup />
         </Route>
+        <Route path="/profile">
+          <Profile />
+        </Route>
         <Route path="/">
           <Home />
         </Route>
       </Switch>
     </Router>
   )
+}
+
+App.propTypes = {
+  loggedIn: PropTypes.bool,
 }
 
 export default App

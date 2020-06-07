8e37c8314a9e3a2cdae0d2e3d991fcbe855f3cb2
shivinquartic <shivin@quartic.ai>
2020-06-07 14:07:06 +0530

    Fixed login issue
    Signed-off-by: shivinquartic <shivin@quartic.ai>

--- systemize/settings.py
+++ systemize/settings.py
@@ -88,12 +88,10 @@ TEMPLATES = [
 REST_USE_JWT = True
 
 JWT_AUTH = {
-
     'JWT_VERIFY': True,
     'JWT_VERIFY_EXPIRATION': True,
-    'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=3000),
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=3000000),
     'JWT_AUTH_HEADER_PREFIX': 'Bearer',
-
 }
 
 REST_FRAMEWORK = {
